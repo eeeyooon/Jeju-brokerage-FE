56080a51c9dc54fd0c3c3a08b204bca9dabdd05d
jiyoon <vywns9978@gmail.com>
2023-09-07 01:51:33 +0900

    [setting] themeProvider로 테마 구축

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./style/theme";
+import { GlobalStyles } from "./style/global";
 import Login from "./pages/Login";
 import Main from "./pages/Main";
 import EmployDetail from "./pages/EmployDetail";
@@ -7,16 +10,19 @@ import EmployList from "./pages/EmployList";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/main" element={<Main />} />
-        <Route path="/" element={<Navigate replace to="/main" />} />
-        <Route path="/employ-detail" element={<EmployDetail />} />
-        <Route path="/employ-write" element={<EmployWrite />} />
-        <Route path="/employ-list" element={<EmployList />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/main" element={<Main />} />
+          <Route path="/" element={<Navigate replace to="/main" />} />
+          <Route path="/employ-detail" element={<EmployDetail />} />
+          <Route path="/employ-write" element={<EmployWrite />} />
+          <Route path="/employ-list" element={<EmployList />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,23 +1,40 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { styled } from "styled-components";
 
 function Main() {
   const navigate = useNavigate();
   return (
-    <div>
+    <MainWrapper>
       <h1>메인 페이지</h1>
-      <button onClick={() => navigate("/login")}>로그인 하기</button>
-      <button onClick={() => navigate("/employ-write")}>
-        구인 공고 글 작성{" "}
-      </button>
+      <NavigateBtn onClick={() => navigate("/login")}>로그인 하기</NavigateBtn>
+      <NavigateBtn onClick={() => navigate("/employ-write")}>
+        구인 공고 글 작성
+      </NavigateBtn>
       <button onClick={() => navigate("/employ-list")}>
         작성한 구인공고 목록 조회
       </button>
       <button onClick={() => navigate("/employ-detail")}>
         구인 공고 자세히보기
       </button>
-    </div>
+    </MainWrapper>
   );
 }
 
 export default Main;
+
+const MainWrapper = styled.div`
+  width: 375px;
+  height: 812px;
+  background-color: ${({ theme }) => theme.color.white};
+  user-select: none;
+`;
+
+const NavigateBtn = styled.button`
+  padding: 10px;
+  border: 1px solid black;
+  font-size: ${({ theme }) => theme.fontSize.body1};
+  font-weight: ${({ theme }) => theme.fontWeight.light};
+  color: ${({ theme }) => theme.color.white};
+  background-color: ${({ theme }) => theme.color.primary_light};
+`;

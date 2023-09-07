9c5e667ef37582d976da814231cf9cd55e32d544
jiyoon <vywns9978@gmail.com>
2023-09-07 17:57:05 +0900

    [style] 버튼 컴포넌트 파일명 수정 및 구인목록 박스 스타일적용

--- src/components/BigButtonLight.jsx
+++ src/components/BigButtonLight.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { styled } from "styled-components";
 
-const SmallButton_light = ({ text }) => {
+const BigButtonLight = ({ text }) => {
   return <ButtonBox>{text}</ButtonBox>;
 };
 
-export default SmallButton_light;
+export default BigButtonLight;
 
 const ButtonBox = styled.button`
-  height: 36px;
-  padding: 10px 24px;
+  height: 48px;
+  padding: 24px 14px;
   display: inline-flex;
   justify-content: center;
   align-items: center;
@@ -17,5 +17,5 @@ const ButtonBox = styled.button`
   border: 1px solid ${({ theme }) => theme.color.primary_normal};
   background-color: ${({ theme }) => theme.color.white};
   color: ${({ theme }) => theme.color.primary_normal};
-  font-size: ${({ theme }) => theme.fontSize.caption1};
+  font-size: ${({ theme }) => theme.fontSize.body1};
 `;

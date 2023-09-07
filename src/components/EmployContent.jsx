1088e3f927c0137c899c56d38b30663b3c6922cb
jiyoon <vywns9978@gmail.com>
2023-09-08 01:40:40 +0900

    [style] 공고 조건 컴포넌트 스타일링

--- src/components/EmployContent.jsx
+++ src/components/EmployContent.jsx
@@ -1,33 +1,145 @@
 import React from "react";
+import { styled } from "styled-components";
 
 function EmployContent() {
   return (
-    <div className="EmployContentWrapper">
-      <div className="EmployConditionBox">
+    <EmployContentWrapper>
+      <EmployConditionWrapper>
         <h2>근무 조건</h2>
-        <div className="WorkDateWrapper">
-          <div className="WorkStartDateBox">
-            <h3>근무 시작일</h3>
+        <WorkDateWrapper>
+          <WorkDateBox>
+            <SubTitleWrapper>
+              <img
+                alt="달력 아이콘"
+                src={process.env.PUBLIC_URL + "/assets/calendar.svg"}
+              />
+              <h3>근무 시작일</h3>
+            </SubTitleWrapper>
             <p>2023-11-10</p>
-          </div>
-          <div className="WorkFinishDateBox">
-            <h3>근무 종료일</h3>
+          </WorkDateBox>
+          <WorkDateBox>
+            <SubTitleWrapper>
+              <img
+                alt="달력 아이콘"
+                src={process.env.PUBLIC_URL + "/assets/calendar.svg"}
+              />
+              <h3>근무 종료일</h3>
+            </SubTitleWrapper>
             <p>2023-12-10</p>
-          </div>
-        </div>
-        <div className="WorkTime">
-          <h3>근무시간</h3>
-          <p>월급 2,600,000</p>
-        </div>
-        <div>
-          <h3>상세 내용</h3>
-          <div className="DetailContent">
-            <p>서귀포에 작은 감귤 농장입니다.</p>
-          </div>
-        </div>
-      </div>
-    </div>
+          </WorkDateBox>
+        </WorkDateWrapper>
+        <WorkTimeWrapper>
+          <SubTitleWrapper>
+            <img
+              alt="시계 아이콘"
+              src={process.env.PUBLIC_URL + "/assets/time.svg"}
+            />
+            <h3>근무 시간</h3>
+          </SubTitleWrapper>
+          <p>09:00 ~ 18:00</p>
+        </WorkTimeWrapper>
+      </EmployConditionWrapper>
+      <SalaryWrapper>
+        <h2>급여 조건</h2>
+        <SalaryText>
+          월급 <span>2,600,000원</span>
+        </SalaryText>
+      </SalaryWrapper>
+      <DetailContentWrapper>
+        <h2>상세 내용</h2>
+        <DetailContent>
+          서귀포에 작은 감귤 농장입니다. 성별무관하고 힘쎈 사람 구합니다. 중식,
+          석식 제공하고 숙박은 제공하지 않습니다.
+        </DetailContent>
+      </DetailContentWrapper>
+    </EmployContentWrapper>
   );
 }
 
 export default EmployContent;
+
+const EmployContentWrapper = styled.div`
+  width: 335px;
+  height: 453px;
+  margin: 20px;
+
+  h2 {
+    font-size: ${({ theme }) => theme.fontSize.body2};
+    font-weight: ${({ theme }) => theme.fontWeight.bold};
+    color: ${({ theme }) => theme.color.grayscale_1C};
+    margin-bottom: 16px;
+  }
+`;
+
+const EmployConditionWrapper = styled.div`
+  width: 335px;
+  height: 146px;
+  margin-bottom: 32px;
+`;
+
+const SubTitleWrapper = styled.div`
+  width: 85px;
+  height: 20px;
+  display: flex;
+  font-size: ${({ theme }) => theme.fontSize.caption1};
+  color: ${({ theme }) => theme.color.grayscale_66};
+  align-items: center;
+  margin-bottom: 4px;
+
+  img {
+    width: 16px;
+    height: 16px;
+  }
+
+  h3 {
+    margin-left: 5px;
+  }
+`;
+
+const WorkDateWrapper = styled.div`
+  width: 335px;
+  height: 46px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const WorkDateBox = styled.div`
+  width: 164px;
+  height: 46px;
+`;
+
+const WorkTimeWrapper = styled.div`
+  width: 335px;
+  height: 46px;
+  margin-top: 16px;
+`;
+
+const DetailContentWrapper = styled.div`
+  width: 335px;
+  height: 180px;
+`;
+
+const SalaryWrapper = styled.div`
+  width: 335px;
+  height: 63px;
+  margin-bottom: 32px;
+`;
+
+const SalaryText = styled.p`
+  width: 164px;
+  height: 25px;
+  font-size: ${({ theme }) => theme.fontSize.body2};
+  color: ${({ theme }) => theme.color.grayscale_1C};
+
+  > span {
+    color: #f54545;
+    font-size: ${({ theme }) => theme.fontSize.body1};
+  }
+`;
+
+const DetailContent = styled.p`
+  color: ${({ theme }) => theme.color.grayscale_1C};
+  font-size: ${({ theme }) => theme.fontSize.caption1};
+  line-height: 150%;
+`;
--- src/pages/employer/EmployerDetail.jsx
+++ src/pages/employer/EmployerDetail.jsx
@@ -8,14 +8,20 @@ function EmployerDetail() {
   return (
     <EmployDetailWrapper>
       <header>
-        <button>뒤로가기 버튼</button>
+        <button>
+          <img
+            alt="뒤로가기 버튼"
+            src={process.env.PUBLIC_URL + "/assets/arrow_back.svg"}
+          />
+        </button>
       </header>
       <PreviewEmployContent />
+      <MarginBox>1</MarginBox>
       <EmployContent />
-      <div className="BtnWrapper">
-        <Button text="수정하기" />
-        <Button text="삭제하기" />
-      </div>
+      <BtnWrapper>
+        <EditBtn>수정하기</EditBtn>
+        <DeleteBtn>삭제하기</DeleteBtn>
+      </BtnWrapper>
     </EmployDetailWrapper>
   );
 }
@@ -27,4 +33,54 @@ const EmployDetailWrapper = styled.div`
   height: 812px;
   background-color: ${({ theme }) => theme.color.white};
   user-select: none;
+
+  header {
+    height: 52px;
+    width: 375px;
+    display: flex;
+    align-items: center;
+    padding-left: 24px;
+    margin-bottom: 12px;
+  }
+`;
+
+const MarginBox = styled.div`
+  height: 60px;
+  color: ${({ theme }) => theme.color.white};
+`;
+
+const BtnWrapper = styled.div`
+  margin-top: 20px;
+  width: 335px;
+  height: 48px;
+  display: flex;
+  justify-content: space-between;
+  margin-left: 20px;
+`;
+
+const EditBtn = styled.button`
+  width: 164px;
+  height: 48px;
+  padding: 10px 24px;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 8px;
+  border: 1px solid ${({ theme }) => theme.color.primary_normal};
+  background-color: ${({ theme }) => theme.color.white};
+  color: ${({ theme }) => theme.color.primary_normal};
+  font-size: ${({ theme }) => theme.fontSize.caption1};
+`;
+
+const DeleteBtn = styled.button`
+  width: 164px;
+  height: 48px;
+  padding: 8px 19px;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.color.primary_normal};
+  color: ${({ theme }) => theme.color.white};
+  font-size: ${({ theme }) => theme.fontSize.caption1};
 `;

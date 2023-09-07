7c60ab8d3955ceef0689f86f7106cd85e3a858fc
이재영 <jyclub10@naver.com>
2023-09-07 22:58:51 +0900

    [Feat] MainPage 코드 받아오기

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,8 +2,19 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
 import PreviewEmployBox from "../components/PreviewEmployBox";
+import axios from "axios";
 
 function Main() {
+  const [businesses, setBusinesses] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://user-app.krampoline.com/k77c33daa3a48a/business")
+      .then((response) => {
+        setBusinesses(response.data);
+      });
+  }, []);
+
   const new_script = (src) => {
     return new Promise((resolve, reject) => {
       const script = document.createElement("script");
@@ -26,39 +37,57 @@ function Main() {
 
     // 스크립트 읽기 완료 후 카카오맵 설정
     my_script.then(() => {
-      console.log("script loaded!!!");
       const kakao = window["kakao"];
       kakao.maps.load(() => {
-        // 현재 위치 정보 가져오기
+        // default 지도 위치 세팅
         if ("geolocation" in navigator) {
           navigator.geolocation.getCurrentPosition(
             (position) => {
               const latitude = position.coords.latitude;
               const longitude = position.coords.longitude;
 
-              const mapContainer = document.getElementById("map");
-              const options = {
-                center: new kakao.maps.LatLng(latitude, longitude), // 현재 위치로 설정
-                level: 3,
-              };
-              const map = new kakao.maps.Map(mapContainer, options);
+              var mapContainer = document.getElementById("map"), // 지도를 표시할 div
+                mapOption = {
+                  center: new kakao.maps.LatLng(latitude, longitude), // 지도의 중심좌표
+                  level: 3, // 지도의 확대 레벨
+                };
 
-              // 마커 설정
-              const markerPosition = new kakao.maps.LatLng(latitude, longitude);
+              var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-              //   var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', // 마커이미지의 주소입니다
-              //   imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
-              //   imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+              var positions = [];
 
-              //     // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-              //     var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
-              //         markerPosition = new kakao.maps.LatLng(37.54699, 127.09598); // 마커가 표시될 위치입니다
-
-              const marker = new kakao.maps.Marker({
-                position: markerPosition,
-                // image: markerImage // 마커이미지 설정
+              businesses.forEach((business) => {
+                positions.push({
+                  title: business.address,
+                  latlng: new kakao.maps.LatLng(
+                    business.longitude,
+                    business.latitude
+                  ),
+                });
               });
-              marker.setMap(map);
+
+              // 마커 이미지의 이미지 주소입니다
+              var imageSrc =
+                "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+
+              for (var i = 0; i < positions.length; i++) {
+                // 마커 이미지의 이미지 크기 입니다
+                var imageSize = new kakao.maps.Size(24, 35);
+
+                // 마커 이미지를 생성합니다
+                var markerImage = new kakao.maps.MarkerImage(
+                  imageSrc,
+                  imageSize
+                );
+
+                // 마커를 생성합니다
+                var marker = new kakao.maps.Marker({
+                  map: map, // 마커를 표시할 지도
+                  position: positions[i].latlng, // 마커를 표시할 위치
+                  title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                  image: markerImage, // 마커 이미지
+                });
+              }
             },
             (error) => {
               console.error("Error getting geolocation:", error);
@@ -69,13 +98,12 @@ function Main() {
         }
       });
     });
-  }, []);
+  }, [businesses]);
 
   return (
     <MainWrapper>
       <div>
         <p> 단기 장기 </p>
-
         <button>구인하기</button>
         <p />
         <button>내가쓴글</button>
@@ -90,7 +118,6 @@ function Main() {
         </div>
         {/* 지도자리  끝*/}
 
-        {/* 상세정보 모달창 */}
         <PreviewEmployBox />
       </div>
     </MainWrapper>
